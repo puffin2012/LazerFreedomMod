318ea3523053682c746fd3b1c1c9b37019c4af64
Brandon <kellatech38@gmail.com>
2015-01-21 08:29:26 +0000

    Updated.

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -15,6 +15,8 @@ public enum TFM_PlayerRank
     SUPER("a " + ChatColor.GOLD + "Super Admin", ChatColor.GOLD + "[SA]"),
     TELNET("a " + ChatColor.DARK_GREEN + "Super Telnet Admin", ChatColor.DARK_GREEN + "[STA]"),
     SENIOR("a " + ChatColor.LIGHT_PURPLE + "Senior Admin", ChatColor.LIGHT_PURPLE + "[SrA]"),
+    EXECUTIVE("a " + ChatColor.RED + "Executive", ChatColor.RED + "[Executive]"),
+    SYSTEMADMIN("a " + ChatColor.DARK_GREEN + "System Administrator", ChatColor.DARK_GREEN + "[Sys-Admin]"),
     OWNER("the " + ChatColor.BLUE + "Owner", ChatColor.BLUE + "[Owner]"),
     CONSOLE("The " + ChatColor.DARK_PURPLE + "Console", ChatColor.DARK_PURPLE + "[Console]");
     private String loginMessage;
